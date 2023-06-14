6b3e4ef7f48c400aca339beea9bdcfb1815307c0
= <=>
2023-06-13 22:13:19 -0300

    Alteração feitas em aula30/05/2023-alterações até o slide 64

---
+++ src/usuario/dto/usuario.dto.ts
@@ -0,0 +1,44 @@
+import { IsEmail, IsInt, MinLength,IsString, IsNotEmpty } from "class-validator";
+import { EmailUnico } from "../validação/email-unico.validator";
+
+export class CriaUsuarioDTO{
+
+@IsString()
+@IsNotEmpty({message:"Nome inválido"})
+nome: string;
+    
+   
+    
+    @IsEmail(undefined, {message: "Email inválido"})
+    @EmailUnico({message:"Já existe usuário com esse email"})
+     email: string;
+    
+
+    
+   @MinLength(6,{message: "Tamanho da senha inválido"})
+    
+ senha: string;
+    
+
+    
+     @IsInt({message: "Idade inválida"})
+    
+     idade: BigInteger;
+    
+    
+    
+    
+     @IsString({message: "Cidade inválida"})
+    
+     cidade: string;
+    
+    
+    
+    
+     @IsString({message: "Telefone inválido"})
+    
+        telefone: string;
+    
+       
+    
+    }
---
+++ src/usuario/usuario.controller.ts
@@ -0,0 +1,43 @@
+import { Body, Controller, Post, Get } from "@nestjs/common";
+
+import {CriaUsuarioDTO} from "./dto/usuario.dto";
+
+import { UsuarioEntity } from "./usuario.entity";
+
+import { UsuariosArmazenados } from "./usuario.dm";
+import{v4 as uuid} from 'uuid';
+
+
+
+@Controller('/usuarios')
+
+export class UsuarioController{
+
+ constructor(private clsUsuariosArmazenados: UsuariosArmazenados){
+ }
+ @Get()
+
+ async RetornoUsuarios(){
+
+ return this.clsUsuariosArmazenados.Usuarios;
+
+ }
+@Post()
+
+ async criaUsuario(@Body() dadosUsuario: CriaUsuarioDTO){
+ var usuario = new UsuarioEntity(uuid(),dadosUsuario.nome,dadosUsuario.idade,dadosUsuario.cidade,dadosUsuario.email,dadosUsuario.telefone,dadosUsuario.senha);
+
+ var retornoUsuario;
+ this.clsUsuariosArmazenados.AdicionarUsuario(usuario)
+
+ retornoUsuario={
+
+ dadosUsuario,
+
+status:'Usuario Criado'
+}
+ return retornoUsuario;
+
+}
+
+}
